fc79bc9df9f4e22c032ebb615d910fb8612631a9
md.rubel <md.rubel@HOAP2406101764A>
2023-02-18 16:19:18 +0600

    Some Coding Add

--- In-MemoryCacheDemo/Controllers/HomeController.cs
+++ In-MemoryCacheDemo/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using In_MemoryCacheDemo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
 
 namespace In_MemoryCacheDemo.Controllers
@@ -7,15 +8,33 @@ namespace In_MemoryCacheDemo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMemoryCache _memoryCash;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMemoryCache memoryCash)
         {
             _logger = logger;
+            _memoryCash = memoryCash;
         }
 
         public IActionResult Index()
         {
+            string cashEntry;
+
+            if (!_memoryCash.TryGetValue("myCashKey",out cashEntry))
+            {
+                cashEntry= TakeLongTime();
+                var cashEntryOption = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+                _memoryCash.Set("myCashKey", cashEntry, cashEntryOption);
+            }
+            ViewBag.Result = cashEntry;
             return View();
+        }
+
+        private string TakeLongTime()
+        {
+            Thread.Sleep(1500);
+            return "I Am Finishing for IO: result here"; ;
         }
 
         public IActionResult Privacy()
